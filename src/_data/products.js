a4d296a13895667827480a209756ac8a811a7b3f
Steven Bailey <stevenbailey@Stevens-Air.attlocal.net>
2023-10-20 18:27:09 -0400

    retailer changes

--- src/_data/products.js
+++ src/_data/products.js
@@ -6,17 +6,8 @@ const productsFolder = path.resolve(__dirname, "../content/products");
 const products = fs
   .readdirSync(productsFolder)
   .filter(name => path.extname(name) === ".json")
-  .map(name => {
-    const filename = path.basename(name, '.json'); // Remove the ".json" extension
-    const rawProductData = require(path.join(productsFolder, name));
-
-    // Create a new product object
-    const productObject = {
-      id: filename,
-      ...rawProductData,
-    };
-
-    return productObject;
-  });
+  .map(name => ({
+    ...require(path.join(productsFolder, name)),
+  }));
 
 module.exports = products;
